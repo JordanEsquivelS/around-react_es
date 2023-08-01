54f73b8a7e1d542ee6316c29e532bba52abb5d87
Jordan Esquivel <jordan11.tauro@gmail.com>
2023-08-01 13:02:59 -0500

    Moficar footer actualice año automatico

--- around-react/src/components/Footer.js
+++ around-react/src/components/Footer.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-function Footer() {
+const Footer = () => {
+  // Obtener el año actual
+  const year = new Date().getFullYear();
+
+  // Crear un nodo de texto con el símbolo de copyright
+  const symbol = String.fromCharCode(169);
+
   return (
     <footer className="footer">
-      <p className="footer__copyright"> 2023 Jordan</p>
+      <p className="footer__copyright">
+        {` ${symbol} ${year} Jordan Esquivel Silva `}
+      </p>
     </footer>
   );
-}
+};
 
 export default Footer;
